9b4db908c3d28d889d9232293df7c2f28275db91
santhoshpixel <santhoshd.pixel@gmail.com>
2022-11-30 17:15:42 +0530

    info data

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -28,6 +29,7 @@ class Kernel extends ConsoleKernel
             // insertApiHomeVisitAreaData();
             // insertApiSampleCollectionCentersData();
             info("Test call");
+            Log::info("TestInfo");
         })->everyMinute();
     }
 
